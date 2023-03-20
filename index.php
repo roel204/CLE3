39f625e99b5e2ca5ea7780a40cf44ffb30fb7a41
Matsdd <Matsdoornhein04@gmail.com>
2023-03-20 14:13:01 +0100

    klaar zo

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
-echo 'AAAAAAAAAAAAAAAAAAAAAAAA'
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Senioren in de Zorg</title>
+    <link rel="stylesheet" href="css/style.css"/>
+    <link rel="icon" href="favicon.png?">
+</head>
+<body>
+<header>
+
+</header>
+<main>
+    <section class="pinboard">
+        <section class="pinboardItem">
+ja
+        </section>
+        <section class="pinboardItem">
+nee
+        </section>
+        <section class="pinboardItem">
+misschien
+        </section>
+        <section class="pinboardItem">
+een beetje
+        </section>
+    </section>
+</main>
+
+<footer>
+
+</footer>
+</body>
+</html>
