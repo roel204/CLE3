38774bf821ed7c82ff5ad01b70c173c4e638b9af
S-witsel <1059566@hr.nl>
2023-03-22 12:55:10 +0100

    intiële code van prog 3 json

--- lindseyMap/algemeneVragen.php
+++ lindseyMap/algemeneVragen.php
@@ -1,3 +1,21 @@
+
+<?php
+//Require functions for actions
+require_once "includes/action.php";
+
+//Based on the existence of the GET parameter, 1 of the 2 functions will be called
+if (!isset($_GET['id'])) {
+    $data = getDishes();
+} else {
+    $data = getDishDetails($_GET['id']);
+}
+
+//Set the header & output JSON so the client will know what to expect.
+header("Content-Type: application/json");
+echo json_encode($data);
+exit;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
---
+++ matsMap/Includes/action.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @return array
+ */
+function getDishes()
+{
+    return [
+        [
+            "id" => 1,
+            "name" => "Pizza",
+            "kitchen" => "Italian",
+        ],
+        [
+            "id" => 2,
+            "name" => "Kale",
+            "kitchen" => "Dutch",
+        ],
+        [
+            "id" => 3,
+            "name" => "Lasagna",
+            "kitchen" => "Italian",
+        ],
+        [
+            "id" => 4,
+            "name" => "Kebab",
+            "kitchen" => "Turkish",
+        ],
+        [
+            "id" => 5,
+            "name" => "Paella",
+            "kitchen" => "Spanish",
+        ]
+    ];
+}
+
+/**
+ * @param $id
+ * @return mixed
+ */
+function getDishDetails($id)
+{
+    $tags = [
+        1 => [
+            "recipe" => "Put it in the oven and go!",
+            "tags" => ['cheese', 'oven']
+        ],
+        2 => [
+            "recipe" => "You can make this delicious Dutch meal by ...",
+            "tags" => ['unox', 'healthy', 'stamppot', 'boerenkool']
+        ],
+        3 => [
+            "recipe" => "Very nice when your grandma prepares this meal",
+            "tags" => ['omnomnom']
+        ],
+        4 => [
+            "recipe" => "Everytime in the city after midnight",
+            "tags" => ['kapsalon', 'tasty', 'meat']
+        ],
+        5 => [
+            "recipe" => "Specialty when on holiday in Spain",
+            "tags" => ['fish']
+        ],
+    ];
+
+    return $tags[$id];
+}
