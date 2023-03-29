b7a2bdc457f14e6cbd8d08564bda9d626a89c971
S-witsel <1059566@hr.nl>
2023-03-29 14:12:29 +0200

    Html files gefixed

--- stijnMap/Includes/action.php
+++ stijnMap/Includes/action.php
@@ -6,132 +6,169 @@ function getLocations()
 {
     return [
         [
+            "id" => 0,
+            "name" => "geen locatie",
+            "zorgLocationUrl" => "#"
+        ],
+        [
             "id" => 1,
-            "name" => "Amsterdam"
+            "name" => "Amsterdam",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=1"
         ],
         [
             "id" => 2,
-            "name" => "Rotterdam"
+            "name" => "Rotterdam",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=2"
         ],
         [
             "id" => 3,
-            "name" => "Den Haag"
+            "name" => "Den Haag",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=3"
         ],
         [
             "id" => 4,
-            "name" => "Utrecht"
+            "name" => "Utrecht",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=4"
         ],
         [
             "id" => 5,
-            "name" => "Eindhoven"
+            "name" => "Eindhoven",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=5"
         ],
         [
             "id" => 6,
-            "name" => "Groningen"
+            "name" => "Groningen",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=6"
         ],
         [
             "id" => 7,
-            "name" => "Tilburg"
+            "name" => "Tilburg",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=7"
         ],
         [
             "id" => 8,
-            "name" => "Almere"
+            "name" => "Almere",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=8"
         ],
         [
             "id" => 9,
-            "name" => "Breda"
+            "name" => "Breda",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=9"
         ],
         [
             "id" => 10,
-            "name" => "Nijmegen"
+            "name" => "Nijmegen",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=10"
         ],
         [
             "id" => 11,
-            "name" => "Apeldoorn"
+            "name" => "Apeldoorn",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=11"
         ],
         [
             "id" => 12,
-            "name" => "Arnhem"
+            "name" => "Arnhem",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=12"
         ],
         [
             "id" => 13,
-            "name" => "Haarlem"
+            "name" => "Haarlem",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=13"
         ],
         [
             "id" => 14,
-            "name" => "Haarlemmermeer"
+            "name" => "Haarlemmermeer",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=14"
         ],
         [
             "id" => 15,
-            "name" => "Enschede"
+            "name" => "Enschede",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=15"
         ],
         [
             "id" => 16,
-            "name" => "Amersfoort"
+            "name" => "Amersfoort",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=16"
         ],
         [
             "id" => 17,
-            "name" => "Zaanstad"
+            "name" => "Zaanstad",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=17"
         ],
         [
             "id" => 18,
-            "name" => "'s-Hertogenbosch"
+            "name" => "'s-Hertogenbosch",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=18"
         ],
         [
             "id" => 19,
-            "name" => "Zwolle"
+            "name" => "Zwolle",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=19"
         ],
         [
             "id" => 20,
-            "name" => "Leeuwarden"
+            "name" => "Leeuwarden",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=20"
         ],
         [
             "id" => 21,
-            "name" => "Zoetermeer"
+            "name" => "Zoetermeer",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=21"
         ],
         [
             "id" => 22,
-            "name" => "Leiden"
+            "name" => "Leiden",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=22"
         ],
         [
             "id" => 23,
-            "name" => "Maastricht"
+            "name" => "Maastricht",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=23"
         ],
         [
             "id" => 24,
-            "name" => "Ede"
+            "name" => "Ede",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=24"
         ],
         [
             "id" => 25,
-            "name" => "Dordrecht"
+            "name" => "Dordrecht",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=25"
         ],
         [
             "id" => 26,
-            "name" => "Westland"
+            "name" => "Westland",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=26"
         ],
         [
             "id" => 27,
-            "name" => "Alphen aan den Rijn"
+            "name" => "Alphen aan den Rijn",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=27"
         ],
         [
             "id" => 28,
-            "name" => "Alkmaar"
+            "name" => "Alkmaar",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=28"
         ],
         [
             "id" => 29,
-            "name" => "Emmen"
+            "name" => "Emmen",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=29"
         ],
         [
             "id" => 30,
-            "name" => "Delft"
+            "name" => "Delft",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=30"
         ],
         [
             "id" => 31,
-            "name" => "Venlo"
+            "name" => "Venlo",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=31"
         ],
         [
             "id" => 32,
-            "name" => "Deventer"
+            "name" => "Deventer",
+            "zorgLocationUrl" => "http://localhost/Jaar1/CLE3/stijnMap/stijnHTML.html?id=32"
         ]
     ];
 }
--- stijnMap/stijn.js
+++ stijnMap/stijn.js
@@ -1,9 +1,13 @@
 window.addEventListener('load', init);
 
 apiUrl = "http://localhost/Jaar1/CLE3/stijnMap/zorgDichtbij.php"
+let locationList = document.getElementById("locaties")
+let locationArray = []
 
-function init()
-{   getLocation()
+function init(){
+    getLocation()
+    let locationlist = document.getElementById("locaties")
+    locationlist.addEventListener("change",locationChoice)
 }
 
 function getLocation() {
@@ -14,14 +18,26 @@ function getLocation() {
             }
             return response.json();
         })
-        .then(ajaxSuccessHandler)
+        .then(ajaxLocationSuccessHandler)
         .catch(ajaxErrorHandler);
 }
 
-function ajaxSuccessHandler(data) {
+function ajaxLocationSuccessHandler(data) {
     console.log(data)
+    for (let location of data){
+        let locationItem = document.createElement("option")
+        locationItem.innerText = location.name
+        locationItem.value = location.id
+        locationList.appendChild(locationItem)
+        locationArray.push(location)
+    }
 }
 
 function ajaxErrorHandler(data) {
+    console.error("er is een fout opgetreden")
     console.error(data)
+}
+
+function locationChoice(){
+    console.log(locationArray[locationList.value].name)
 }
