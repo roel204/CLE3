c5167fe7f920cabd68ee897cf92de4cff695a7b0
Roel-H <roelhoogendoorn01369@gmail.com>
2023-03-29 11:24:58 +0200

    roel edit

--- roelMap/roel.js
+++ roelMap/roel.js
@@ -1,13 +1,26 @@
 window.addEventListener('load', init);
 
-
+let apiUrl = "https://raw.githubusercontent.com/Roel-H/CLE3/master/roelMap/rechtInZorg.php"
 
 function init()
 {
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText)
+            }
+            return response.json()
+        })
+        .then(createList)
+        .catch(ajaxErrorHandler)
+}
+
+function createList(e) {
+    console.log("YIPEE")
+    let block = document.createElement("div")
 
 }
 
-function createList() {
-    let block = document.createElement("div")
-
+function ajaxErrorHandler(e) {
+    console.error(e)
 }
