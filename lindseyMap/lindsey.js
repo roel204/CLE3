cd0d159ef773492c929c81784d297438a4d22967
lindsey <lindseytaak123@gmail.com>
2023-04-03 15:28:41 +0200

    hennietalk

--- lindseyMap/lindsey.js
+++ lindseyMap/lindsey.js
@@ -4,5 +4,22 @@ window.addEventListener('load', init);
 
 function init()
 {
+    hennieTalk("Hier kunt u verschillende veelgestelde vragen zien, klik op één van de vragen voor een antwoord.")
+}
 
+function hennieTalk(text) {
+
+    const message = text;
+    const delay = 10;
+
+    let i = 0;
+    let interval = setInterval(() => {
+        if (message[i] === ' ') {
+            box.innerHTML += '&nbsp;';
+        } else {
+            box.innerText += message[i];
+        }
+        i++;
+        if (i >= message.length) clearInterval(interval);
+    }, delay);
 }
