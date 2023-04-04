18ff460e728357e7889afffb869833bfc34707bf
Matsdd <Matsdoornhein04@gmail.com>
2023-04-04 12:18:48 +0200

    Nieuwe update

--- matsMap/mats.js
+++ matsMap/mats.js
@@ -1,8 +1,82 @@
 window.addEventListener('load', init);
 
+let pinboard = document.getElementById("pinboard")
+let detailDialog;
+let detailContent;
+
+
 
 
 function init()
 {
+    hennieTalk()
 
+    imageUrls = ["matsimg/Ontruiming.png", "matsimg/Pijn.png", "matsimg/Ziekte.png"]
+    dataUrls = ["https://www.google.nl", "https://www.google.nl", "https://www.google.nl"]
+
+    for (let imageUrl of imageUrls) {
+        const image = document.createElement("img");
+        image.src = imageUrl;
+        image.classList.add('pinboardItem')
+        image.setAttribute("data-url", dataUrls[imageUrls.indexOf(imageUrl)]);
+        pinboard.appendChild(image);
+    }
+
+    document.addEventListener("click", imageClickHandler)
+}
+
+function imageClickHandler(e) {
+    if (e.target.className !== 'hennie') {
+        if (e.target.nodeName === "IMG") {
+            detailWindow();
+        }
+    }
+}
+
+function hennieTalk() {
+
+    const message =
+"Hier vind U informatie over wat U het best eerst kan doen in verschilldende situaties die te maken hebben met Uw veiligheid.";
+    const delay = 10;
+
+    let i = 0;
+    let interval = setInterval(() => {
+        if (message[i] === ' ') {
+            box.innerHTML += '&nbsp;';
+        } else {
+            box.innerText += message[i];
+        }
+        i++;
+        if (i >= message.length) clearInterval(interval);
+    }, delay);
+}
+
+function detailWindow(e) {
+    let clickedItem = e.target;
+
+    if (clickedItem.nodeName !== 'BUTTON'){
+        return;
+    }
+
+    detailDialog.showModal();
+
+    let pokemon = pokemonData[clickedItem.dataset.id];
+
+    detailContent.innerHTML= '';
+
+    let title = document.createElement('h1');
+    title.innerHTML = `${pokemon.name} (#${pokemon.id})`;
+    detailContent.appendChild(title);
+
+    let image = document.createElement('img');
+    image.src = pokemon.sprites.other['official-artwork'].front_shiny;
+    image.classList.add('pokemon-card');
+    detailContent.appendChild(image);
+
+    let types = document.createElement('h3');
+    if (pokemon.types.length === 2) {
+        types.innerHTML = pokemon.types['0'].type.name + ' / ' + pokemon.types['1'].type.name;}
+    if (pokemon.types.length === 1) {
+        types.innerHTML = pokemon.types['0'].type.name}
+    detailContent.appendChild(types);
 }
