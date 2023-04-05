3ca2ab6215680f95c0d22d171e0621754473676a
lindsey <lindseytaak123@gmail.com>
2023-04-05 10:12:51 +0200

    lindseys shit

--- lindseyMap/lindsey.js
+++ lindseyMap/lindsey.js
@@ -5,6 +5,21 @@ apiUrl = "http://localhost/Jaar1/CLE3/lindseyMap/algemeneVragen.php"
 function init()
 {
     hennieTalk("Hier kunt u verschillende veelgestelde vragen zien, klik op één van de vragen voor gegeven antwoorden.")
+    apiFetch(apiUrl)
+    favoriteItems = JSON.parse(localStorage.getItem('favorite')) || [];
+    hennieTalk("Op deze pagina kunt u meer leren over uw rechten. Klik op een van de titels voor meer info.")
+}
+
+function apiFetch(api) {
+    fetch(api)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText)
+            }
+            return response.json()
+        })
+        .then(createList)
+        .catch(ajaxErrorHandler)
 }
 
 function hennieTalk(text) {
