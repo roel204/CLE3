836ac9401cdff3f896ead7ddae62c6eb3eafde5a
Matsdd <Matsdoornhein04@gmail.com>
2023-04-05 10:12:25 +0200

    Nieuwe update

--- matsMap/Includes/action.php
+++ matsMap/Includes/action.php
@@ -2,33 +2,92 @@
 /**
  * @return array
  */
-function getDishes()
+function getDataOntruiming()
 {
     return [
         [
-            "id" => 1,
-            "name" => "Pizza",
-            "kitchen" => "Italian",
+            "id" => "1",
+            "catagory" => "Ontruiming",
+            "title" => "In geval van brand",
+            "description" => "",
+            "source" => "https://www.dezorgbrandveilig.nl",
         ],
         [
-            "id" => 2,
-            "name" => "Kale",
-            "kitchen" => "Dutch",
+            "id" => "2",
+            "catagory" => "Ontruiming",
+            "title" => "In geval van inbraak",
+            "description" => "",
+            "source" => "https://www.dezorgbrandveilig.nl",
         ],
         [
-            "id" => 3,
-            "name" => "Lasagna",
-            "kitchen" => "Italian",
+            "id" => "3",
+            "catagory" => "Ontruiming",
+            "title" => "In geval van een gaslek",
+            "description" => "",
+            "source" => "https://www.dezorgbrandveilig.nl",
         ],
         [
-            "id" => 4,
-            "name" => "Kebab",
-            "kitchen" => "Turkish",
+            "id" => "4",
+            "catagory" => "Pijn",
+            "title" => "Hoofdpijn",
+            "description" => "",
+            "source" => "https://www.dezorgbrandveilig.nl",
         ],
         [
-            "id" => 5,
-            "name" => "Paella",
-            "kitchen" => "Spanish",
+            "id" => "5",
+            "catagory" => "Pijn",
+            "title" => "Keelpijn",
+            "description" => "",
+            "source" => "https://www.dezorgbrandveilig.nl",
+        ],
+        [
+            "id" => "6",
+            "catagory" => "Pijn",
+            "title" => "Buikpijn",
+            "description" => "",
+            "source" => "https://www.dezorgbrandveilig.nl",
+        ],
+        [
+            "id" => "7",
+            "catagory" => "Pijn",
+            "title" => "Spierpijn in armen of benen",
+            "description" => "",
+            "source" => "https://www.dezorgbrandveilig.nl",
+        ],
+        [
+            "id" => "8",
+            "catagory" => "Pijn",
+            "title" => "Pijn in boven en/of onderrug",
+            "description" => "",
+            "source" => "https://www.dezorgbrandveilig.nl",
+        ],
+        [
+            "id" => "9",
+            "catagory" => "Pijn",
+            "title" => "Pijn in de nek & schouders",
+            "description" => "",
+            "source" => "https://www.dezorgbrandveilig.nl",
+        ],
+        [
+            "id" => "10",
+            "catagory" => "Ziekte",
+            "title" => "Griep",
+            "description" => "",
+            "source" => "https://www.dezorgbrandveilig.nl",
+        ],
+        [
+            "id" => "11",
+            "catagory" => "Ziekte",
+            "title" => "Corona",
+            "description" => "",
+            "source" => "https://www.dezorgbrandveilig.nl",
+        ],
+        [
+            "id" => "12",
+            "catagory" => "Ziekte",
+            "title" => "Anders",
+            "description" => "",
+            "source" => "https://www.dezorgbrandveilig.nl",
         ]
     ];
 }
